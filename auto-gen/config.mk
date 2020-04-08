1b24b9fc1450944f2dc8a7f64a60c1119900267e
jhkim <jhkim-voc@visiononchip.com>
2020-04-08 17:15:26 +0100

    change mk files for cygwin build

--- auto-gen/config.mk
+++ auto-gen/config.mk
@@ -2,12 +2,13 @@ REPLACE_ME= XXXXXX
 OUTDIR    = $(REPLACE_ME)
 #_______________________________________________________________________________
 #                       PROJECTS GLOBAL CONFIG                      IGNORE ABOVE
-#VERBOSE   = 1
+VERBOSE   = 1
 DEBUGMODE = 1
 
 G_MACROS  =
 G_INCS    = $(TOPDIR)util .
-G_LIBDIRS = $(TOPDIR)util
+#G_LIBDIRS = $(TOPDIR)util
+G_LIBDIRS = $(TOPDIR)out_debug/util
 G_LIBS    = util
 G_CPPFLAGS= -Wall -Werror -Wextra -Wmissing-prototypes -Wstrict-prototypes -Wunused-value -Wno-unused-parameter -Wformat \
   -Wfloat-equal -Wshadow -Wpointer-arith -Wcast-qual -Wcast-align -Wwrite-strings -Wredundant-decls
--- auto-gen/topdir.mk
+++ auto-gen/topdir.mk
@@ -4,6 +4,7 @@ OUTDIR    = $(REPLACE_ME)
 ifndef TOPDIR
 TOPDIR := $(dir $(realpath $(lastword $(MAKEFILE_LIST))))
 $(info make help		View help info)
-#$(info TOPDIR $(TOPDIR))
+$(info TOPDIR $(TOPDIR))
+$(info REPLACE_ME $(REPLACE_ME))
 endif
 export TOPDIR
